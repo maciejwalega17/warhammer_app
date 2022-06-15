cee104e705f613b804f59b52971011f5c9ff35f0
maciejwalega17 <maciejwalega17@gmail.com>
2022-06-15 23:12:02 +0200

    selecting options now change state of app

--- src/components/App.js
+++ src/components/App.js
@@ -5,29 +5,46 @@ const App = () => {
 	const [selectedPhase, setSelectedPhase] = useState('Command');
 	const [selectedArmy, setSelectedArmy] = useState('Tyranids');
 
-	const phaseList = ['Command', 'Movement', 'Psychic', 'Shooting', 'Fight', 'Morale']
-	const armyList = ['Tyranids', 'Orks']
+	const phaseList = [
+		'Command',
+		'Movement',
+		'Psychic',
+		'Shooting',
+		'Fight',
+		'Morale',
+	];
+	const armyList = ['Tyranids', 'Orks'];
+
+	const callbackArmy = (value) => {
+		setSelectedArmy(value);
+	};
+	const callbackPhase = (value) => {
+		setSelectedPhase(value);
+	};
 
 	return (
 		<>
 			<div className='wrapper'>
-				<Selector type='Phase Select' list={phaseList}></Selector>
-				<Selector type='Army Select' list={armyList}></Selector>
+				<Selector type='Phase Select' list={phaseList} callback={callbackPhase}></Selector>
+				<Selector type='Army Select' list={armyList} callback={callbackArmy}></Selector>
 			</div>
 			<div className='wrapper'>
 				<button>Back</button>
 				<button>Forward</button>
 			</div>
+			<div className='wrapper'>
+				{selectedPhase}
+				{selectedArmy}
+			</div>
 		</>
 	);
 };
 
-export default App
+export default App;
 
 //<input type="text" placeholder="dodaj zadanie" value={this.state.text} onChange={this.handleText} />
- // <input type="checkbox" checked={this.state.checked} id="important" onChange={this.handleCheckbox} />
+// <input type="checkbox" checked={this.state.checked} id="important" onChange={this.handleCheckbox} />
 
- 
 //  handleText = (e) => {
 //   this.setState({
 //     text: e.target.value
@@ -39,7 +56,3 @@ export default App
 //     checked: e.target.checked
 //   })
 // }
-
-
-
-
--- src/components/selector.jsx
+++ src/components/selector.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Option from './option';
 
-const Selector = ({ type, list }) => {
-	const optionList = list.map((el) => {
-		return <Option value={el}></Option>;
+const Selector = ({ type, list, callback }) => {
+	const optionList = list.map((el, index) => {
+		return <Option value={el} key={index}></Option>;
 	});
 
 	return (
 		<>
-			<select name={type} id=''>
+			<select name={type} onChange={(e) => callback(e.target.value)}>
 				{optionList}
 			</select>
 		</>
