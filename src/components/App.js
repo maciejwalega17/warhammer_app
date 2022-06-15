c257d291efe6e9cbe0511736e5eb50b54d48e7c8
maciejwalega17 <maciejwalega17@gmail.com>
2022-06-15 22:49:56 +0200

    cleaning not used elements

---
+++ src/components/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import Selector from './selector';
+
+const App = () => {
+	const [selectedPhase, setSelectedPhase] = useState('Command');
+	const [selectedArmy, setSelectedArmy] = useState('Tyranids');
+
+	const phaseList = ['Command', 'Movement', 'Psychic', 'Shooting', 'Fight', 'Morale']
+	const armyList = ['Tyranids', 'Orks']
+
+	return (
+		<>
+			<div className='wrapper'>
+				<Selector type='Phase Select' list={phaseList}></Selector>
+				<Selector type='Army Select' list={armyList}></Selector>
+			</div>
+			<div className='wrapper'>
+				<button>Back</button>
+				<button>Forward</button>
+			</div>
+		</>
+	);
+};
+
+export default App
+
+//<input type="text" placeholder="dodaj zadanie" value={this.state.text} onChange={this.handleText} />
+ // <input type="checkbox" checked={this.state.checked} id="important" onChange={this.handleCheckbox} />
+
+ 
+//  handleText = (e) => {
+//   this.setState({
+//     text: e.target.value
+//   })
+// }
+
+// handleCheckbox = (e) => {
+//   this.setState({
+//     checked: e.target.checked
+//   })
+// }
+
+
+
+
