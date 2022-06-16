675dfab6780392a2c874c8ae9c9fa2649b798391
maciejwalega17 <maciejwalega17@gmail.com>
2022-06-16 11:19:34 +0200

    downloaded dom elements

---
+++ stratagem_conventer/converter.js
@@ -0,0 +1,10 @@
+const nameInput = document.querySelector('.name');
+const costInput = document.querySelector('.cost');
+const categoryInput = document.querySelector('.category');
+const phaseInput = document.querySelector('.phase');
+const whoseInput = document.querySelector('.whose');
+const textInput = document.querySelector('.text');
+const responseInput = document.querySelector('.response');
+
+const submitButton = document.querySelector('.submit');
+const copyButton = document.querySelector('.copy');
