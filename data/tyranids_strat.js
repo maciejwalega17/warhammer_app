1006b3376e3a79e257e5e794ef6132f7fb0ee0a7
maciejwalega17 <maciejwalega17@gmail.com>
2022-06-16 13:24:33 +0200

    added data folder

---
+++ data/tyranids_strat.js
@@ -0,0 +1,31 @@
+const testObj = {
+	stratagem1: {
+		name: 'stratagem1',
+		cost: '1cp',
+		category: 'requisition',
+		phase: 'fight',
+		whose: 'enemy',
+		text: 'qrwrrrrrrrrrr wwwwwwwwwwww ssssss r',
+		keywords: 'csm, adrenal glands',
+	},
+	stratagem2: {
+		name: 'stratagem2',
+		cost: '2cp',
+		category: 'epic deed',
+		phase: 'movement',
+		whose: 'both',
+		text: 'asf f fqfq fwqerqrwqrqw',
+		keywords: 'tyranid, genestealer',
+	},
+	stratagem3: {
+		name: 'stratagem3',
+		cost: '1/2cp',
+		category: 'wargear',
+		phase: 'command',
+		whose: 'mine',
+		text: 'asdfsafas asfsafs tqwtq',
+		keywords: 'monster',
+	},
+};
+
+export default testObj;
