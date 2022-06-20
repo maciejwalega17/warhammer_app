71988bdc6ceecfef5dbea7a808d09cdb8d6af72b
maciejwalega17 <maciejwalega17@gmail.com>
2022-06-20 14:48:52 +0200

    universalized buttonGenerator

--- src/functions/buttonGenerator.jsx
+++ src/functions/buttonGenerator.jsx
@@ -2,90 +2,20 @@ import React from 'react';
 import Button from '../components/button';
 
 const buttonGenerator = (cost, callback, name) => {
-	if (cost === '1/2') {
-		return (
-			<>
-				<Button
-					name={`1 CP`}
-					onClick={() => {
-						callback(name, 1);
-					}}
-				/>
-				<Button
-					name={`2 CP`}
-					onClick={() => {
-						callback(name, 2);
-					}}
-				/>
-			</>
-		);
-	} else if (cost === '1/2/3') {
-		return (
-			<>
-				<Button
-					name={`1 CP`}
-					onClick={() => {
-						callback(name, 1);
-					}}
-				/>
-				<Button
-					name={`2 CP`}
-					onClick={() => {
-						callback(name, 2);
-					}}
-				/>
-				<Button
-					name={`3 CP`}
-					onClick={() => {
-						callback(name, 3);
-					}}
-				/>
-			</>
-		);
-	} else if (cost === '2/3') {
-		return (
-			<>
-				<Button
-					name={`2 CP`}
-					onClick={() => {
-						callback(name, 2);
-					}}
-				/>
-				<Button
-					name={`3 CP`}
-					onClick={() => {
-						callback(name, 3);
-					}}
-				/>
-			</>
-		);
-	} else if (cost === '2/4') {
-		return (
-			<>
-				<Button
-					name={`2 CP`}
-					onClick={() => {
-						callback(name, 2);
-					}}
-				/>
-				<Button
-					name={`4 CP`}
-					onClick={() => {
-						callback(name, 4);
-					}}
-				/>
-			</>
-		);
-	} else {
-		return (
+	const costSplit = cost.split('/');
+	const btnArr = [];
+	costSplit.forEach((el) => {
+		btnArr.push(
 			<Button
-				name={`${cost} CP`}
+				key={name}
+				name={`${el} CP`}
 				onClick={() => {
-					callback(name, cost);
+					callback(name, el);
 				}}
 			/>
 		);
-	}
+	});
+	return btnArr;
 };
 
 export default buttonGenerator;
