7591303e061926ad8926765903f015a7647e998d
maciejwalega17 <maciejwalega17@gmail.com>
2022-06-20 14:49:02 +0200

    added sorting for armyList

--- src/data/armyList.js
+++ src/data/armyList.js
@@ -1,3 +1,16 @@
-const armyList = ['Tyranids', 'Orks', 'Aeldari', 'Adepta Sororitas', 'Adeptus Custodes', 'T\u2019au Empire', 'Aeldari (Harlequins)', 'Adeptus Mechanicus', 'Necrons', 'Chaos Knights', 'Death Guard', 'Grey Knights'];
-
-export default armyList
+const armyListPrototype = [
+	'Tyranids',
+	'Orks',
+	'Aeldari',
+	'Adepta Sororitas',
+	'Adeptus Custodes',
+	'T\u2019au Empire',
+	'Aeldari (Harlequins)',
+	'Adeptus Mechanicus',
+	'Necrons',
+	'Chaos Knights',
+	'Death Guard',
+	'Grey Knights',
+];
+const armyList = armyListPrototype.sort();
+export default armyList;
