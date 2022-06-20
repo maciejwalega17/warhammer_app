9d51a2fdd8b5793178b89f54064078921f30b6b1
maciejwalega17 <maciejwalega17@gmail.com>
2022-06-20 18:35:50 +0200

    added counter styles

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import Button from './button';
 
-import '../styles/counter.css'
+import '../styles/counter.css';
 
 const Counter = ({ counter, currency, onClickAdd, onClickSub }) => {
 	return (
 		<>
-			<div>
-				<div>
-					<p>{counter} {currency}</p>
+			<div className='flex-row'>
+				<div className='counter-container flex-row'>
+					<h3>
+						{counter} {currency}
+					</h3>
 				</div>
-				<Button name={`- 1 ${currency}`} onClick={() => onClickSub()} />
-				<Button name={`+ 1 ${currency}`} onClick={() => onClickAdd()} />
+				<div className='counter-btn-container flex-column'>
+					<Button name={`- 1 ${currency}`} onClick={() => onClickSub()} />
+					<Button name={`+ 1 ${currency}`} onClick={() => onClickAdd()} />
+				</div>
 			</div>
 		</>
 	);
