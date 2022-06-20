4bed71d8d3ed5000ef5d2e7d599aab70a0861d25
maciejwalega17 <maciejwalega17@gmail.com>
2022-06-20 21:08:15 +0200

    exported functiond for swapping mode

--- src/components/settings.jsx
+++ src/components/settings.jsx
@@ -1,60 +1,29 @@
 import React from 'react';
 import Button from './button';
+import switchLight from '../functions/switchLight';
+import switchDark from '../functions/switchDark';
 
 const Settings = ({ mode, callback }) => {
-	const switchLight = () => {
-		document.documentElement.style.setProperty(
-			'--bg-primary-color',
-			'rgb(200, 200, 200)'
-		);
-		document.documentElement.style.setProperty(
-			'--bg-secondary-color',
-			'rgb(240, 240, 240)'
-		);
-		document.documentElement.style.setProperty(
-			'--bg-tertiary-color',
-			'rgb(250, 250, 250)'
-		);
-		document.documentElement.style.setProperty(
-			'--bg-quaternary-color',
-			'rgb(220, 220, 220)'
-		);
-		document.documentElement.style.setProperty('--text-color', 'rgb(0, 0, 0)');
+	const switchToLight = () => {
+		switchLight();
 		callback(false);
 	};
-	const switchDark = () => {
-		document.documentElement.style.setProperty(
-			'--bg-primary-color',
-			'rgb(43, 43, 43)'
-		);
-		document.documentElement.style.setProperty(
-			'--bg-secondary-color',
-			'rgb(66, 66, 66)'
-		);
-		document.documentElement.style.setProperty(
-			'--bg-tertiary-color',
-			'rgb(121, 121, 121)'
-		);
-		document.documentElement.style.setProperty(
-			'--bg-quaternary-color',
-			'rgb(51, 51, 51)'
-		);
-		document.documentElement.style.setProperty(
-			'--text-color',
-			'rgb(255, 255, 255)'
-		);
+	const switchToDark = () => {
+		switchDark();
 		callback(true);
 	};
 
 	return (
 		<>
 			{mode ? (
-				<Button name='Switch to Light Mode' onClick={switchLight} />
+				<Button name='Switch to Light Mode' onClick={switchToLight} />
 			) : (
-				<Button name='Switch to Dark Mode' onClick={switchDark} />
+				<Button name='Switch to Dark Mode' onClick={switchToDark} />
 			)}
 		</>
 	);
 };
 
 export default Settings;
+
+
---
+++ src/functions/switchDark.js
@@ -0,0 +1,23 @@
+const switchDark = () => {
+	document.documentElement.style.setProperty(
+		'--bg-primary-color',
+		'rgb(43, 43, 43)'
+	);
+	document.documentElement.style.setProperty(
+		'--bg-secondary-color',
+		'rgb(66, 66, 66)'
+	);
+	document.documentElement.style.setProperty(
+		'--bg-tertiary-color',
+		'rgb(121, 121, 121)'
+	);
+	document.documentElement.style.setProperty(
+		'--bg-quaternary-color',
+		'rgb(51, 51, 51)'
+	);
+	document.documentElement.style.setProperty(
+		'--text-color',
+		'rgb(255, 255, 255)'
+	);
+};
+export default switchDark;
