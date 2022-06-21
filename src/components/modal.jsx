64d10665c1b5abc8f17d62484e8644c9c4b1d892
maciejwalega17 <maciejwalega17@gmail.com>
2022-06-21 14:29:31 +0200

    added shadow to modal

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -7,12 +7,14 @@ import ModalContent from './modal_content';
 const Modal = ({ content, onClick, show }) => {
 	return (
 		<>
-			<div className={`${show ? 'active' : 'inactive'} modal`}>
-				<div className='modal-container'>
-					<ModalContent content={content} />
-				</div>
-				<div className='modal-btn-container'>
-					<Button name='Close' onClick={onClick} />
+			<div className={`${show ? 'active' : 'inactive'} shadow`}>
+				<div className='modal flex-column'>
+					<div className='modal-container'>
+						<ModalContent content={content} />
+					</div>
+					<div className='modal-btn-container'>
+						<Button name='Close' onClick={onClick} />
+					</div>
 				</div>
 			</div>
 		</>
